bb1980d4b4e6560cf65a3f35afc160c99cdfa6db
ahvShiro <arthurshiro123@gmail.com>
2024-05-13 18:29:40 -0300

    Exclui palavras que contém ou não letras do input

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ while True:
         continue
 
 
-letras_nao_na_palavra = []
 letras_posicao_errada = []
 letras_posicao_certa = []
 
@@ -28,6 +27,7 @@ for i in range(1, 5 + 1):
     temp = input(f"{Fore.GREEN}Letras na posição certa [{i}ª coluna]: {Style.RESET_ALL}").upper()
     letras_posicao_certa.append("".join(OrderedDict.fromkeys(temp)))
 
+
 linhas = []
 with open(dicio_palavras, "r") as file:
     for i in file:
@@ -36,6 +36,22 @@ with open(dicio_palavras, "r") as file:
             linhas.append(i.upper())
 
 
-# for palavras in linhas:
+for palavra in linhas:
+    palavra_errada = False
+
+    # Descarta todas as palavras que contém letras cinzas
+    for letra_cinza in letras_nao_na_palavra:
+        if(letra_cinza in palavra):
+            palavra_errada = True
+    
+    # Descarta todas as palavras que não contém letras verdes ou amarelas
+    for letra_necessaria in "".join(letras_posicao_errada + letras_posicao_certa):
+        if(letra_necessaria not in palavra):
+            palavra_errada = True
+    
     
 
+    if(palavra_errada):
+        continue
+
+    print(palavra)
