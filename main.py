942a0a0039e03cc9a3d02690ab8ff7e0182b8c75
ahvShiro <arthurshiro123@gmail.com>
2024-05-13 17:24:05 -0300

    Adicionado código que pega input do usuário

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-from sys import argv
+from colorama import Fore, Style
+from collections import OrderedDict
 
-# Checa se o usuário colocou um dicionário como argumento
-if (len(argv) != 2):
-    print(f"Uso: {argv[0]} <dicionário>")
+# PEDE O NOME DO ARQUIVO DO DICIONÁRIO  
+while True:
+    dicio_palavras = input("Nome do arquivo do dicionário (txt): ")
+    try:
+        with open(dicio_palavras, 'r'):
+            break
+    except FileNotFoundError:
+        continue
 
+
+letras_nao_na_palavra = []
+letras_posicao_errada = []
+letras_posicao_certa = []
+
+# PEDE LETRAS CINZAS
+letras_nao_na_palavra = input("Letras que não estão na palavra: ")
+
+# PEDE LETRAS AMARELAS
+for i in range(1, 5 + 1):
+    temp = input(f"{Fore.YELLOW}Letras na posição errada [{i}ª coluna]: {Style.RESET_ALL}").upper()
+    letras_posicao_errada.append("".join(OrderedDict.fromkeys(temp)))
+
+# PEDE LETRAS VERDES
+for i in range(1, 5 + 1):
+    temp = input(f"{Fore.GREEN}Letras na posição certa [{i}ª coluna]: {Style.RESET_ALL}").upper()
+    letras_posicao_certa.append("".join(OrderedDict.fromkeys(temp)))
 
 linhas = []
-with open(argv[1], "r") as f:
-    for i in f:
+with open(dicio_palavras, "r") as file:
+    for i in file:
         i = i.strip()
         if len(i.strip()) == 5:
             linhas.append(i.upper())
 
-for palavras in linhas:
-    print("")
+
+# for palavras in linhas:
+    
+
